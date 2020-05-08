50cab527dfc23af2feac54597e19b13102215e29
karmaKiller3352 <alievsul@gmail.com>
2020-05-08 14:07:57 +0300

    Updates

---
+++ precache-manifest.c3723ac10db5c5d89ac62c210696c116.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d89e9973b75341155d10fe3e88b0298",
+    "url": "/vk-app/index.html"
+  },
+  {
+    "revision": "77fc5fc404576e6defe1",
+    "url": "/vk-app/static/css/main.e3f87101.chunk.css"
+  },
+  {
+    "revision": "c1bb6f0e09f3979b6a2a",
+    "url": "/vk-app/static/js/2.31e7ced9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/vk-app/static/js/2.31e7ced9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77fc5fc404576e6defe1",
+    "url": "/vk-app/static/js/main.742e2636.chunk.js"
+  },
+  {
+    "revision": "e5056cc6ee5e329e10a5",
+    "url": "/vk-app/static/js/runtime-main.a98f73b0.js"
+  }
+]);
